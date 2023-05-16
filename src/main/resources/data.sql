c13965c38c40d5668200e370c5135b493989962f
LEEHYUNBOK <gusqhrzkq@naver.com>
2023-05-16 15:20:57 +0900

    Feat : test data add

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,12 @@
 use christian;
 
-insert into member(member_id, password, name) values ('admin', '1234','test');
-insert into member_roles(member_member_id, roles) values ('admin', 'ADMIN');
+insert into member(email, password, name) values ('admin', '1234', 'test');
+insert into member_roles(member_id, roles) values (1, 'ADMIN');
+
+insert into class_type(name, teacher) values ('test1', 1);
+insert into class_type(name, teacher) values ('test2', 1);
+
+insert into student(name, age, memo, photo, sex, class_id) values ('test1', '12', 'etst', 'none', 'male', 1);
+insert into student(name, age, memo, photo, sex, class_id) values ('test2', '12', '22etst', 'none', 'male', 1);
+insert into student(name, age, memo, photo, sex, class_id) values ('test3', '12', 'etst', 'none', 'male', 2);
+insert into student(name, age, memo, photo, sex, class_id) values ('test4', '12', 'etst', 'none', 'male', 2);
