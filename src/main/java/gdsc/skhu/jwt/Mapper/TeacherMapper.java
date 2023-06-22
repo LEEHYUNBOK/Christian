5e2776302b379fb63f71e8047e0d54d42c35569b
hyunbokLee <l.hn.bk0905@gmail.com>
2023-06-22 16:25:03 +0900

    Feat(#1) : TeacherMapper 생성

--- src/main/java/gdsc/skhu/jwt/Mapper/TeacherMapper.java
+++ src/main/java/gdsc/skhu/jwt/Mapper/TeacherMapper.java
@@ -2,6 +2,8 @@ package gdsc.skhu.jwt.Mapper;
 
 import gdsc.skhu.jwt.domain.DTO.TeacherDTO;
 import gdsc.skhu.jwt.domain.Teacher;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -9,6 +11,16 @@ import org.mapstruct.factory.Mappers;
 @Mapper(componentModel = "spring")
 public interface TeacherMapper {
     TeacherMapper INSTANCE = Mappers.getMapper(TeacherMapper.class);
+
+    @BeforeMapping
+    default void beforTest() {
+        System.out.println("befor");
+    }
+
+    @AfterMapping
+    default void afterTest() {
+        System.out.println("after");
+    }
 
     @Mapping(source = "teacher.classType.name", target = "className")
     @Mapping(target = "studentCount", expression = "java(teacher.getClassType().getStudents().size())")
