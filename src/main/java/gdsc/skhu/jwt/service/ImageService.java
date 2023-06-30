bc91698df1fe20b3251a4b636d144e5809f64859
hyunbokLee <l.hn.bk0905@gmail.com>
2023-06-30 14:49:24 +0900

    Feat(#2) : image delete and insertImage fucntion return change string

--- src/main/java/gdsc/skhu/jwt/service/ImageService.java
+++ src/main/java/gdsc/skhu/jwt/service/ImageService.java
@@ -4,6 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 @Slf4j
@@ -16,7 +20,7 @@ public class ImageService {
 
 //    private final Storage storage;
 
-    public void insertImage(MultipartFile image, String whoIsIt, String whatIsJob) {
+    public String insertImage(MultipartFile image, String whoIsIt, String whatIsJob) {
         log.info("updateMemberChallengeInfo = ");
         // !!!!!!!!!!!이미지 업로드 관련 부분!!!!!!!!!!!!!!!
         String imageName = whatIsJob+"/" + whoIsIt + ".jpg"; // Google Cloud Storage에 저장될 파일 이름
@@ -24,12 +28,25 @@ public class ImageService {
         log.info("UUID" + imageName + " " + ext);
 
         // Cloud에 이미지 업로드
-//        BlobInfo blobInfo = storage.create(
-//                BlobInfo.newBuilder(bucketName, imageName)
-//                        .setContentType(ext)
-//                        .build(),
-//                image.getInputStream()
-//        );
-
+//        try {
+//            BlobInfo blobInfo = storage.create(
+//                    BlobInfo.newBuilder(bucketName, imageName)
+//                            .setContentType(ext)
+//                            .build(),
+//                    image.getInputStream()
+//            );
+//        } catch (IOException e) {
+//            throw new RuntimeException();
+//        }
+        return "https://storage.googleapis.com/"+"버킷이름/"+whatIsJob+"/" + whoIsIt + ".jpg";
     }
+
+
+//    public void deleteImage(String filePath) {
+//        boolean isDelete = storage.delete(BlobId.of(bucketName, filePath));
+//
+//        if (!isDelete) {
+//            throw new GCSFIleNotFoundException();
+//        }
+//    }
 }
